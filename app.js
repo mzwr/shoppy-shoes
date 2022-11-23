c02feec549bdc55e7b5541cfe4d9e20a73c12623
abdesslem <107839818+mzwr@users.noreply.github.com>
2022-11-23 14:37:13 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+
+let openn = document.querySelector('.open')
+let navbar = document.getElementById('nav')
+
+
+  openn.addEventListener('click', function(){
+    navbar.classList.toggle('show')
+  })
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+  
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+      // Default parameters
+      slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
+  
+  });
